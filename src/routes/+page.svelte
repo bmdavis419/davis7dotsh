13ecb0dfc3b8f4a617d3ad995524c5d6f0919602
Benjamin Davis <davis.benjamin41902@gmail.com>
2025-04-28 19:17:19 -0700

    logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import SocialLinks from '$lib/components/SocialLinks.svelte';
+	import logo from '$lib/svg/my-logo.svg';
 </script>
 
 <svelte:head>
@@ -12,6 +13,9 @@
 
 <main class="z-10 px-3 text-center">
 	<article class="prose prose-neutral prose-invert pb-16 text-left">
+		<div class="mb-8 flex justify-center">
+			<img src={logo} alt="Ben Davis Logo" class="w-24" />
+		</div>
 		<h1 class="mb-0 pb-0">Ben Davis</h1>
 		<p class="m-0">SF based software engineer and YouTuber</p>
 		<p>
