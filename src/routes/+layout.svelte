bdb0fa4fb4edb5f1788a53ad98f4fa56ad5e1a54
Ben Davis <davis.benjamin41902@gmail.com>
2025-09-28 01:58:07 -0700

    moving back to vercel it's easier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <div
-	class="dark relative min-h-screen w-full bg-gradient-to-br from-gray-950 via-gray-900 to-blue-900/80 font-geist-mono text-white"
+	class="dark relative min-h-screen w-full bg-gradient-to-br from-gray-950 via-gray-900 to-gray-800 font-geist-mono text-white"
 >
 	<div
-		class="z-5 fixed inset-0 bg-[radial-gradient(ellipse_at_top_right,rgba(59,130,246,0.1),transparent_80%)]"
+		class="z-5 fixed inset-0 bg-[radial-gradient(ellipse_at_top_right,rgba(107,114,128,0.1),transparent_80%)]"
 	></div>
 	<div
-		class="z-5 fixed inset-0 bg-[radial-gradient(ellipse_at_bottom_left,rgba(29,78,216,0.1),transparent_80%)]"
+		class="z-5 fixed inset-0 bg-[radial-gradient(ellipse_at_bottom_left,rgba(75,85,99,0.1),transparent_80%)]"
 	></div>
 
 	<div class="relative z-10 flex h-full w-full justify-center overflow-auto py-8">
